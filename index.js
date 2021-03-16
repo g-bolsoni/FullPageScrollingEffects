bb6cd2c746ded76d02a2af877732b5d2c09f3d4e
g-bolsoni <giovanebolsoni2@gmail.com>
2021-03-16 00:25:23 -0300

    page for, buid the integration with github

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// ----------------------------------------------------------------
+// Navbar responsive
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,3 +9,23 @@ function myFunction() {
   }
 }
 
+// ----------------------------------------------------------------
+// integração com o github
+
+for(let i=1; i <=9; i++){
+  const box = document.createElement('div');
+  box.classList.add('box');
+  document.querySelector('.git').appendChild(box);
+}
+
+// ----------------------------------------------------------------
+// api
+fetch('https://api.github.com/users/g-bolsoni')
+.then(res => res.json())
+.then(data => {
+  console.log(data);
+  console.log(data.company);
+  console.log(data.repos_url);
+
+  document.getElementsByClassName('box').innerHTML = data.bio;
+})
